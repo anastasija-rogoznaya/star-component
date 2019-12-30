1d60585c2ce3947b3895046b489ba0c7af715d47
anastasija-zogoznaya <59358243+anastasija-zogoznaya@users.noreply.github.com>
2019-12-30 17:06:35 +0300

    commit

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/PluginRating" {
+    var PluginRating: string;
+    export default PluginRating;
+}
+declare module "@salesforce/resourceUrl/RatingPlugin" {
+    var RatingPlugin: string;
+    export default RatingPlugin;
+}
